cfea92c649c8727e60a6a80b233df33f9e751608
travis mills <travismills82@gmail.com>
2016-09-15 10:46:38 +0800

    add fm radio
    add fm radio

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,7 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=24M@0-0xffffffff rcupdate.rcu_expedited=1 androidboot.selinux=permissive
 BOARD_KERNEL_SEPARATED_DT := true
-BOARD_DTBTOOL_ARGS := -3
+# BOARD_DTBTOOL_ARGS := -3
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x02000000
@@ -129,6 +129,9 @@ BOARD_FLASH_BLOCK_SIZE := 262144
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+
+# FM
+AUDIO_FEATURE_ENABLED_FM := true
 
 # Init
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc0/
