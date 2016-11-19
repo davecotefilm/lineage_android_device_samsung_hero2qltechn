fbfdcb00ffda1eba7cd0a29b3abde47cf028e58f
travis mills <travismills82@gmail.com>
2016-11-19 19:07:02 +0800

    remove enhanced nfc

--- cm.mk
+++ cm.mk
@@ -17,9 +17,6 @@ $(call inherit-product, device/samsung/hero2qltechn/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hero2qltechn
 PRODUCT_NAME := cm_hero2qltechn
