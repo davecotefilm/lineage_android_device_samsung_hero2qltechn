92c340371da5bf8c696569fd008579cba76d5f3e
travis mills <travismills82@gmail.com>
2017-01-05 23:03:19 +0800

    hero2qltechn: update our device .mk
    Let's leave the files where stock put them but add links to the location our stack uses.

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2016 The CyanogenMod Project
+# Copyright 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,5 +28,128 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),hero2qltechn)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+ADSP_IMAGES := \
+    adsp.b00 adsp.b01 adsp.b02 adsp.b03 adsp.b04 adsp.b05 adsp.b06 adsp.b07 \
+    adsp.b08 adsp.b09 adsp.mdt
+
+ADSP_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware-modem/image/,$(notdir $(ADSP_IMAGES)))
+$(ADSP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "ADSP firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/adsp/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(ADSP_SYMLINKS)
+
+MBA_IMAGES := \
+    mba.mbn
+
+MBA_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware-modem/image/,$(notdir $(MBA_IMAGES)))
+$(MBA_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "MBA firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/radio/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(MBA_SYMLINKS)
+
+MODEM_IMAGES := \
+    modem.b00 modem.b01 modem.b02 modem.b03 modem.b04 modem.b05 \
+    modem.b06 modem.b07 modem.b08 modem.b09 modem.b10 modem.b11 \
+    modem.b12 modem.b13 modem.b15 modem.b16 modem.b17 modem.b18 \
+    modem.b19 modem.b20 modem.mdt
+
+MODEM_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware-modem/image/,$(notdir $(MODEM_IMAGES)))
+$(MODEM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Modem firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/radio/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(MODEM_SYMLINKS)
+
+SLPI_IMAGES := \
+    slpi.b00 slpi.b01 slpi.b02 slpi.b03 slpi.b04 slpi.b05 slpi.b06 \
+    slpi.b07 slpi.b08 slpi.b09 slpi.b10 slpi.b11 slpi.b12 slpi.b13 \
+    slpi.b14 slpi.b15 slpi.mdt
+
+SLPI_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/image/,$(notdir $(SLPI_IMAGES)))
+$(SLPI_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "SLPI firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/slpi/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(SLPI_SYMLINKS)
+
+VENUS_IMAGES := \
+    venus.b00 venus.b01 venus.b02 venus.b03 venus.b04 venus.mdt
+
+VENUS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/image/,$(notdir $(VENUS_IMAGES)))
+$(VENUS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "VENUS firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/venus/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VENUS_SYMLINKS)
+
+WIDEVINE_IMAGES := \
+    windevine.b00 windevine.b01 windevine.b02 windevine.b03 windevine.b04 \
+    windevine.b05 windevine.b06 windevine.mdt
+
+WIDEVINE_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/image/,$(notdir $(WIDEVINE_IMAGES)))
+$(WIDEVINE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "WIDEVINE firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/etc/firmware/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(WIDEVINE_SYMLINKS)
+
+.PHONY: RFS_LINK_PROCESSING
+RFS_LINK_PROCESSING: $(LOCAL_INSTALLED_MODULE)
+	mkdir -p $(TARGET_OUT)/rfs/apq/gnss/readonly
+	mkdir -p $(TARGET_OUT)/rfs/msm/adsp/readonly
+	mkdir -p $(TARGET_OUT)/rfs/msm/mpss/readonly
+	mkdir -p $(TARGET_OUT)/rfs/mdm/adsp/readonly
+	mkdir -p $(TARGET_OUT)/rfs/mdm/mpss/readonly
+	mkdir -p $(TARGET_OUT)/rfs/mdm/sparrow/readonly
+	ln -sf /persist/hlos_rfs/shared $(TARGET_OUT)/rfs/apq/gnss/hlos
+	ln -sf /data/tombstones/modem $(TARGET_OUT)/rfs/apq/gnss/ramdumps
+	ln -sf /firmware $(TARGET_OUT)/rfs/apq/gnss/readonly/firmware
+	ln -sf /persist/rfs/apq/gnss $(TARGET_OUT)/rfs/apq/gnss/readwrite
+	ln -sf /persist/rfs/shared $(TARGET_OUT)/rfs/apq/gnss/shared
+	ln -sf /persist/hlos_rfs/shared $(TARGET_OUT)/rfs/mdm/adsp/hlos
+	ln -sf /data/tombstones/lpass $(TARGET_OUT)/rfs/mdm/adsp/ramdumps
+	ln -sf /firmware $(TARGET_OUT)/rfs/mdm/adsp/readonly/firmware
+	ln -sf /persist/rfs/mdm/adsp $(TARGET_OUT)/rfs/mdm/adsp/readwrite
+	ln -sf /persist/rfs/shared $(TARGET_OUT)/rfs/mdm/adsp/shared
+	ln -sf /persist/hlos_rfs/shared $(TARGET_OUT)/rfs/mdm/mpss/hlos
+	ln -sf /data/tombstones/modem $(TARGET_OUT)/rfs/mdm/mpss/ramdumps
+	ln -sf /firmware $(TARGET_OUT)/rfs/mdm/mpss/readonly/firmware
+	ln -sf /persist/rfs/mdm/mpss $(TARGET_OUT)/rfs/mdm/mpss/readwrite
+	ln -sf /persist/rfs/shared $(TARGET_OUT)/rfs/mdm/mpss/shared
+	ln -sf /persist/hlos_rfs/shared $(TARGET_OUT)/rfs/mdm/sparrow/hlos
+	ln -sf /data/tombstones/sparrow $(TARGET_OUT)/rfs/mdm/sparrow/ramdumps
+	ln -sf /firmware $(TARGET_OUT)/rfs/mdm/sparrow/readonly/firmware
+	ln -sf /persist/rfs/mdm/sparrow $(TARGET_OUT)/rfs/mdm/sparrow/readwrite
+	ln -sf /persist/rfs/shared $(TARGET_OUT)/rfs/mdm/sparrow/shared
+	ln -sf /persist/hlos_rfs/shared $(TARGET_OUT)/rfs/msm/adsp/hlos
+	ln -sf /data/tombstones/lpass $(TARGET_OUT)/rfs/msm/adsp/ramdumps
+	ln -sf /firmware $(TARGET_OUT)/rfs/msm/adsp/readonly/firmware
+	ln -sf /persist/rfs/msm/adsp $(TARGET_OUT)/rfs/msm/adsp/readwrite
+	ln -sf /persist/rfs/shared $(TARGET_OUT)/rfs/msm/adsp/shared
+	ln -sf /persist/hlos_rfs/shared $(TARGET_OUT)/rfs/msm/mpss/hlos
+	ln -sf /data/tombstones/modem $(TARGET_OUT)/rfs/msm/mpss/ramdumps
+	ln -sf /firmware $(TARGET_OUT)/rfs/msm/mpss/readonly/firmware
+	ln -sf /firmware/wsd $(TARGET_OUT)/rfs/msm/mpss/readonly/wsd
+	ln -sf /persist/rfs/msm/mpss $(TARGET_OUT)/rfs/msm/mpss/readwrite
+	ln -sf /persist/rfs/shared $(TARGET_OUT)/rfs/msm/mpss/shared
+
+ALL_DEFAULT_INSTALLED_MODULES += RFS_LINK_PROCESSING
 
 endif
